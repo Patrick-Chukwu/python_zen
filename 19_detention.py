2ed74c931cfa5f8f8c22a317b4a35bb36df69fb9
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2025-04-06 18:49:38 +0100

    feat: update file

--- 19_detention.py
+++ 19_detention.py
@@ -1,1 +1,11 @@
 # To loop through a code a specific number if times, we can use for loop and the range function.
+
+# The range function generates a sequence of numbers, typically starting from 0 and ending at a particular number(exclusive).
+for i in range(6):
+    print(i*2)
+
+
+
+
+for i in range(101):
+    print("I will not use Snapchat in class")
