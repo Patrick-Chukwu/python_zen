5e1e31539a5c9620e6fe382e474baff2290eafd9
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2025-04-27 12:07:52 +0100

    feat: solve fizzbuzz

---
+++ 20_99_bottles.py
@@ -0,0 +1,6 @@
+# String Interpolation
+
+#String interpolation is a process of substituting values of variables into placeholders in a string.
+
+for i in range(99, 0, -1):
+  print(f'{i} bottles of beer on the wall \n{i} bottles of beer \nTake one down, pass it around \n{i - 1} bottles of beer on the wall \n\n')
